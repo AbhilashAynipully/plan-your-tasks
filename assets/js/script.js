16f237e3985b384b1294bcf4a4fdf34bd8e87f26
Abhilash Aynipully <abhidigitally@gmail.com>
2023-06-05 12:46:11 +0000

    Add structure to functions for planner actions

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function themeChanger() {}
+
+function taskIdentifier () {}
+
+function firstTask(){}
+
+function newTask(){}
+
+function resetTable(){}
+
+function createEditPopup (selectedTask){}
+
+function highlightTask () {}
+
+function markDone () {}
+
+function deleteTask () {}
+
+function refreshTable () {}
